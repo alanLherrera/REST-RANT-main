fdab569239445213b1498d492414c4a366aa2914
alanLherrera <108435763+alanLherrera@users.noreply.github.com>
2022-11-04 22:41:12 -0700

    fixes.

--- views/home.jsx
+++ views/home.jsx
@@ -1,11 +1,19 @@
 const React = require('react')
-const Def = require('./default')
+const Def = require('./places/default')
 
 function home () {
   return (
     <def>
       <main>
         <h1>HOME</h1>
+        <div>
+          <img src="images/eggs.jpeg" />
+        </div>
+        <a href="/places">
+        <button className="btn-primary">Places Page</button>
+        </a>
+
+
       </main>
     </def>
   )
